2268ebe0fef439d8dff5d5109add2ef8ee59fe9a
StomaticSP8 <79164240+StomaticSP8@users.noreply.github.com>
2021-11-20 15:01:17 +0100

    primo commit

---
+++ Progetto_PO_DEC/src/main/java/com/example/Progetto_PO_DEC/ProgettoPoDecApplication.java
@@ -0,0 +1,13 @@
+package com.example.Progetto_PO_DEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPoDecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPoDecApplication.class, args);
+	}
+
+}
---
+++ Progetto_PO_DEC/src/test/java/com/example/Progetto_PO_DEC/ProgettoPoDecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Progetto_PO_DEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPoDecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
